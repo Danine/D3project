01e36f522c60f54467a5ac8812dcfb4661e54a59
Tyson <381980920@qq.com>
2019-03-24 23:48:04 +0800

    update

--- homework1/test.py
+++ homework1/test.py
@@ -1,7 +1,11 @@
 import csv
+from pyecharts import Bar
+
 read = csv.reader(open('D:\GitHub\VisualizationProject\homework1\census2000.csv','r'))
 census = []
 for line in read:
     census.append(line)
-for i in census:
-    print(i[2])
+
+
+bar = Bar("测试")
+bar.add("男")
