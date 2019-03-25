dd3d2ef66c3ab3f92d29468439e86840bb66d682
Tyson <381980920@qq.com>
2019-03-25 11:12:44 +0800

    update

--- homework1/test.py
+++ homework1/test.py
@@ -8,15 +8,22 @@ for line in read:
 census.pop(0)
 
 attr = []
-v1 = []; v2 = []
+v119 = []; v219 = []; v120 = []; v220 = []
 for i in census:
     if i[0] == '1' and i[1] == '1900':
         attr.append(int(i[2]))
-        v1.append(int(i[3]))
+        v119.append(int(i[3]))
+    elif i[0] == '1' and i[1] == '2000':
+        v120.append(int(i[3]))
     elif i[0] == '2' and i[1] == '1900':
-        v2.append(int(i[3]))
+        v219.append(int(i[3]))
+    elif i[0] == '2' and i[1] == '2000':
+        v220.append(int(i[3]))
 
-bar = Bar("1900")
-bar.add("男", attr, v1, mark_point = ["average"])
-bar.add("女", attr, v2, mark_line = ["min", "max"])
+bar = Bar(title = "1900-2000", subtitle="男女人数比", width = 1400, height = 700)
+bar.add("1900男", attr, v119)
+bar.add("1900女", attr, v219)
+bar.add("2000男", attr, v120)
+bar.add("2000女", attr, v220)
+bar.show_config()
 bar.render()
